7a4c0e56dbb3d449fc4c6455f39439c3a5691f79
Alireza Hosseini <alireza.hoseini20031382@gmail.com>
2022-12-07 15:42:01 +0330

    Chrome class and the two methods for creating the chrome driver and navigating to the url were created

---
+++ src/VideoDownloader/Chrome.java
@@ -0,0 +1,19 @@
+package VideoDownloader;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public class Chrome {
+
+	private WebDriver CreateChromeDriver() {
+		System.setProperty("webdriver.chrome.driver","G://Selenium jar and drivers//chromedriver/chromedriver.exe");
+		WebDriver chromeDriver = new ChromeDriver();
+		return chromeDriver;
+	}
+	
+	private void NavigateToURL(String url) {
+		WebDriver chromeDriver = CreateChromeDriver();
+		chromeDriver.get(url);
+	}
+}
--- src/VideoDownloader/Main.java
+++ src/VideoDownloader/Main.java
@@ -1,10 +1,12 @@
 package VideoDownloader;
 
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		
 	}
 
 }
