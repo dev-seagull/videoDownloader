de42ead06c31c74ae094626bba67bff1d77e67b8
Alireza Hosseini <alireza.hoseini20031382@gmail.com>
2022-12-07 16:46:31 +0330

    FindVideoLinksByXPATH method was added and some lines were refactored

--- src/VideoDownloader/Chrome.java
+++ src/VideoDownloader/Chrome.java
@@ -1,23 +1,35 @@
 package VideoDownloader;
 
+import java.util.List;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 
 public class Chrome {
 
-	private WebDriver CreateChromeDriver() {
+	
+	public WebDriver CreateChromeDriver() {
 		System.setProperty("webdriver.chrome.driver","G://Selenium jar and drivers//chromedriver/chromedriver.exe");
 		WebDriver chromeDriver = new ChromeDriver();
 		return chromeDriver;
 	}
 	
-	private void NavigateToURL(String url) {
-		WebDriver chromeDriver = CreateChromeDriver();
-		chromeDriver.get(url);
+	public void NavigateToURL(String url, WebDriver driver) {
+		driver.get(url);
 	}
 	
-	private void MaximizeWindow(WebDriver driver) {
+	public void MaximizeWindow(WebDriver driver) {
 		driver.manage().window().maximize();
 	}
+	
+	public List<WebElement> FindVideoLinksByXPATH(String url,WebDriver driver) {
+		NavigateToURL(url,driver);
+		List<WebElement> links = driver.findElements(By.xpath("//*[@id=\"video-title\"]"));
+		return links;
+	}
+	
+	
 }
