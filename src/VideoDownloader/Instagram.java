81b07ca8040ae5808a4d60a2386ef9597d2ae45b
Alireza Hosseini <alireza.hoseini20031382@gmail.com>
2022-12-24 20:44:56 +0330

    Getting unlimited youtube videos feature was added

--- src/VideoDownloader/Instagram.java
+++ src/VideoDownloader/Instagram.java
@@ -1,5 +1,63 @@
 package VideoDownloader;
 
-public class Instagram {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Scanner;
+
+
+public class Instagram {
+	
+	
+    Scanner input = new Scanner(System.in);
+	
+	Chrome chrome = new Chrome();
+	
+	
+	public void Login(WebDriver driver) {
+	
+		driver.get("http://www.instagram.com");
+		
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(30));
+		
+		
+		WebElement username = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[name='username']")));
+		WebElement password = wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector("input[type='password']")));
+		
+		
+		System.out.print("Enter your username:");
+		String usernameInput = input.nextLine();
+
+		System.out.print("Enter your Password:");
+		String passwordInput = input.nextLine();
+		
+		username.clear();
+		username.sendKeys(usernameInput);
+		driver.manage().timeouts().implicitlyWait(4,TimeUnit.SECONDS);
+		
+		password.clear();
+		password.sendKeys(passwordInput);
+		driver.manage().timeouts().implicitlyWait(7,TimeUnit.SECONDS);
+		
+		WebElement loginButton = driver.findElement(By.cssSelector("button[type='submit'"));
+		loginButton.click();
+		driver.manage().timeouts().implicitlyWait(12,TimeUnit.SECONDS);
+		
+		
+		//WebElement not_now = driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[1]/div/div/div/div[1]/div[1]/div[2]/section/main/div/div/div/div/button"));
+		//not_now.click();
+		//driver.manage().timeouts().implicitlyWait(13,TimeUnit.SECONDS);
+	}
+	
+	
+	public void SearchAndGetVideos(String url, WebDriver driver) {
+		
+	}
+	
 }
--- src/VideoDownloader/Youtube.java
+++ src/VideoDownloader/Youtube.java
@@ -1,8 +1,11 @@
 package VideoDownloader;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -11,12 +14,30 @@ public class Youtube{
 	
 	Chrome chrome = new Chrome();
 	
-	public void SearchAndGetVideos(String url, WebDriver driver) {
-		List<WebElement> links =  chrome.FindVideoLinksByXPATH(url, driver);
-
-		for(int i=0; i<links.size() ; i++) {
-			System.out.println(links.get(i).getAttribute("href"));
+	public void SearchAndGetVideos(String searchURL,WebDriver driver) throws InterruptedException{
+		driver.get(searchURL);
+		
+		
+		int counter=0;
+		while(counter<100) {
+			driver.findElement(By.cssSelector("body")).sendKeys(Keys.CONTROL, Keys.END);
+			
+			List<WebElement> links = driver.findElements(By.xpath("//*[@id=\"video-title\"]"));
+			
+			
+			for(int i=0; i<links.size() ; i++) {
+				if(links.get(i).getAttribute("href") != null) {
+					System.out.println(links.get(i).getAttribute("href"));
+					counter = counter +1;
+					if(counter == 100) {
+						break;
+					}
+				}
+			}
 		}
+	
+				
+		System.out.println(counter+ " youtube videos found");
 	}
 	
 	
