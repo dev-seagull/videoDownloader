f976e2e4e75ad0edfb3dfb6b2367fd0894da0b22
Alireza Hosseini <alireza.hoseini20031382@gmail.com>
2023-01-18 10:28:31 +0330

    the error handling feature was added

--- src/VideoDownloader/Main.java
+++ src/VideoDownloader/Main.java
@@ -2,6 +2,7 @@ package VideoDownloader;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import org.apache.commons.cli.CommandLine;
@@ -11,7 +12,6 @@ import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
-import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
@@ -33,43 +33,71 @@ public class Main {
 		    final Options options = new Options();
 		    options.addOption(new Option("d","download",false,"Downloading videos(essential)"));
 		    options.addOption(new Option("k","keyword",true,"Giving the keyword(essential)"));
+		    
+		    
 		    options.addOption(new Option("p","path",true,"where you want to save the videos(optional)"));
 		    options.addOption(new Option("n","number",true,"Number of videos you want to save(optional)"));
+		   
 		    
-		    
-		    CommandLineParser parser = new DefaultParser();
-			CommandLine cmd = parser.parse(options, args);
-		    
-			
-			String userInput = cmd.getOptionValue("k");
-			String destination = cmd.getOptionValue("p");
-			String numberOfVideos = cmd.getOptionValue("n");
-			
-			
-			ChromeOptions chromeOptions = new ChromeOptions();
-			chromeOptions.addArguments("--headless");
-			ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
-			
-			
-			Youtube youtube = new Youtube();
-			
-			String[] essentialFlags = {"d","k"}; 
-			
-			for(int i=0;i< essentialFlags.length;i++) {
-				if(!cmd.hasOption(essentialFlags[i])) {
-					System.out.println("You didn't give the "+essentialFlags[i]+ " flag");
-					printHelp(options);
-					System.exit(-1);
+		    try {
+		    	CommandLineParser parser = new DefaultParser();
+				CommandLine cmd = parser.parse(options, args);
+				
+				
+				String userInput = cmd.getOptionValue("k");
+				String destination = cmd.getOptionValue("p");
+				String numberOfVideos = cmd.getOptionValue("n");
+		
+				
+				ChromeOptions chromeOptions = new ChromeOptions();
+				chromeOptions.addArguments("--headless");
+				ChromeDriver chromeDriver = new ChromeDriver(chromeOptions);
+				
+				
+				Youtube youtube = new Youtube();
+				
+				
+				Option[] userInputOptions  = cmd.getOptions();
+				for(Option userInputOption: userInputOptions) {
+					String option = userInputOption.getOpt();
+					if(!options.hasOption(option)) {
+						System.out.println(option + " is not recognized as a flag");
+					}
 				}
-			}
+				
+				
+				String[] essentialFlags = {"d","k"}; 
+				
+				for(int i=0;i< essentialFlags.length;i++) {
+					if(!cmd.hasOption(essentialFlags[i])) {
+						System.out.println("You didn't give the "+essentialFlags[i]+ " flag");
+						printHelp(options);
+						System.exit(-1);
+					}
+				}
+					
+				
+				if(cmd.hasOption("d")) {
+					youtube.collectYoutubeVideos(userInput,chromeDriver,Integer.parseInt(numberOfVideos),destination);
+				}
+		    }
+		    
+		    
+		    catch(Exception err) {
+		    	
+		    	System.out.println(err.getMessage());	
+		    
+		    }
+		    		
+		    
+		    
+		    
+	
+		 
+		 
 			
 			
 			
-			
-			
-			if(cmd.hasOption("d")) {
-				youtube.collectYoutubeVideos(userInput,chromeDriver,Integer.parseInt(numberOfVideos),destination);
-			}
 			
 
 	}
